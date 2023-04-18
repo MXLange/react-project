8a0f6f4098bc78078d66d5503fa82b8369721e43
MXLange <lange_sp@hotmail.com>
2023-04-17 23:05:12 -0300

    final 17/04/2023

--- src/pages/TodoList/index.jsx
+++ src/pages/TodoList/index.jsx
@@ -12,36 +12,61 @@ const taskSchema = yup.object({
     date: yup.string().matches(/\d{2}\/\d{2}\/\d{4}/i, " date must be dd/mm/yyyy")
 })
 
+const editedSchema = yup.object({
+    editId: yup.string(),
+    editName: yup.string().required("Please insert a name"),
+    editDescription: yup.string().required("Please insert a description"),
+    editDate: yup.string().matches(/\d{2}\/\d{2}\/\d{4}/i, " date must be dd/mm/yyyy")
+});
+
 
 const TodoList = () => {
 
     const [tasks, setTasks] = useState([]);
+    const [editLayer, setEditLayer] = useState(false);
 
-    const { register, reset, handleSubmit, watch, formState: { errors } } = useForm({
+    const { control: controlCreate, register: registerCreate, reset: resetCreate, handleSubmit: handleSubmitCreate, setValue: setValueCreate, formState: { errors: errorsCreate } } = useForm({
         resolver: yupResolver(taskSchema)
     });
-    
 
-    function onSubmit(data) {
-        if (localStorage.getItem("array")) {
-            setTasks(JSON.parse(localStorage.getItem("array")));
-        }
+    const { control: controlEdit, register: registerEdit, reset: resetEdit, handleSubmit: handleSubmitEdit, setValue: setValueEdit, getValues, formState: { errors: errorsEdit } } = useForm({
+        resolver: yupResolver(editedSchema)
+    });
+
+    function onSubmit(taskSchema) {
         if(tasks.length > 0) {
-            data.id = parseInt(tasks[tasks.length -1].id) + 1;
+            taskSchema.id = parseInt(tasks[tasks.length -1].id) + 1;
         } else {
-            data.id = 0;
+            taskSchema.id = 0;
         }
-        toString(data.id);
-        setTasks([...tasks, data]);
-        reset();
+        toString(taskSchema.id);
+        setTasks([...tasks, taskSchema]);
+        resetCreate();
+    }
+
+    function onEdit() {
+        const id = getValues("editId");
+        const tasks = JSON.parse(localStorage.getItem("array"));
+        tasks[id].name = getValues("editName");
+        tasks[id].description = getValues("editDescription");
+        tasks[id].date = getValues("editDate");
+        localStorage.setItem("array", JSON.stringify(tasks));
+        setEditLayer(false);
+        setTasks([...tasks]);
+        resetEdit();
     }
     
-    function editTask(item) {
-        
+    const editTask = (id) => {
+        const toChange = (tasks.find((item) => item.id === id));
+        setEditLayer(true);
+        setValueEdit("editId", toChange.id)
+        setValueEdit("editName", toChange.name);       
+        setValueEdit("editDescription", toChange.description);       
+        setValueEdit("editDate", toChange.date);
     }
     
     function deleteTask(id) {
-        const newList = tasks.filter((task) => parseInt(task.id) != parseInt(id));
+        const newList = tasks.filter((task) => parseInt(task.id) !== parseInt(id));
         setTasks(newList);
     }
     
@@ -49,20 +74,32 @@ const TodoList = () => {
         localStorage.setItem("array", JSON.stringify(tasks));
     }, [tasks])
 
+    useEffect(() => {
+        if(editLayer === true) {
+            const modal = document.getElementById("edit-screen");
+    
+            modal.addEventListener("click", function(e) {
+                if(e.target === this)
+                    setEditLayer(false); 
+            });
+        } 
+    }, [editLayer])
+
+
     return(
         <div id="todo-list">
             <div id="todo-form">
                 <h1>Create your task</h1>
-                <form className="task" onSubmit={handleSubmit(onSubmit)}>
+                <form className="task" onSubmit={handleSubmitCreate(onSubmit)}>
                     <label htmlFor="todo-name">Name: </label>
-                    <span className="error">{errors?.name?.message}</span>
-                    <input type="text" id="todo-name" {...register("name")}/>
+                    <span className="error">{!editLayer && errorsCreate?.name?.message}</span>
+                    <input type="text" id="todo-name" {...registerCreate("name")}/>
                     <label htmlFor="todo-description">Description: </label>
-                    <span className="error">{errors?.description?.message}</span>
-                    <textarea id="todo-description" cols="30" rows="10" {...register("description")}></textarea>
+                    <span className="error">{!editLayer && errorsCreate?.description?.message}</span>
+                    <textarea id="todo-description" cols="30" rows="10" {...registerCreate("description")}></textarea>
                     <label htmlFor="todo-deadline">Deadline: </label>
-                    <span className="error">{errors?.date?.message}</span>
-                    <input type="text" id="todo-deadline" placeholder="dd/mm/yyyy" {...register("date")}/>
+                    <span className="error">{!editLayer && errorsCreate?.date?.message}</span>
+                    <input type="text" id="todo-deadline" placeholder="dd/mm/yyyy" {...registerCreate("date")}/>
                     <button id="todo-button">Create</button>
                 </form>
             </div>
@@ -73,8 +110,10 @@ const TodoList = () => {
                         <p className="name">Name: {item.name}</p>
                         <p className="description">Description: {item.description}</p>
                         <p className="deadline">Deadline: {item.date}</p>
-                        <button type="button" id="edit-task" onClick={editTask(item.description)}>Edit</button>
-                        <button type="button" id="delete-task" onClick={() => deleteTask(item.id)}>Delete</button>
+                        <div id="task-div-buttons">
+                            <button type="button" id="edit-task" onClick={() => editTask(item.id)}>Edit</button>
+                            <button type="button" id="delete-task" onClick={() => deleteTask(item.id)}>Delete</button>
+                        </div>
                     </li>
                 )) : (
                     <div id="empty-list">
@@ -82,6 +121,26 @@ const TodoList = () => {
                     </div>
                 )}
             </div>
+                {editLayer ? (
+                    <div id="edit-screen">
+                        <div id="edit-screen-content">
+                            <h1>Edit your form</h1>
+                            <form className="task-edit" onSubmit={handleSubmitEdit(onEdit)}>
+                                <label htmlFor="todo-edit-name">Name: </label>
+                                <span className="error">{errorsEdit?.editName?.message}</span>
+                                <input type="text" id="todo-edit-name" {...registerEdit("editName")}/>
+                                <label htmlFor="todo-edit-description">Description: </label>
+                                <span className="error">{errorsEdit?.editDescription?.message}</span>
+                                <textarea id="todo-edit-description" cols="30" rows="10" {...registerEdit("editDescription")} ></textarea>
+                                <label htmlFor="todo-edit-deadline">Deadline: </label>
+                                <span className="error">{errorsEdit?.editDate?.message}</span>
+                                <input type="text" id="todo-edit-deadline" placeholder="dd/mm/yyyy" {...registerEdit("editDate")}/>
+                                <button id="todo-button">Save</button>
+                            </form>
+                        </div>
+                    </div>
+                ) : <div></div>
+                }
         </div>
     );
 }
